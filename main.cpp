5d77471d1d07740081c2a1d954a3e08a80331264
coffeediaries <119687013+coffeediaries@users.noreply.github.com>
2022-12-07 11:53:26 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum = 0;
+
+  for (int i = 1; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+cout << "The sum of the multiples of 3 and 5 below 1000 is: " << sum << endl;
+}
