4ee0b24c57e4644da9cdb628bd2674c2283898af
coffeediaries <119687013+coffeediaries@users.noreply.github.com>
2022-12-14 11:03:59 +0800

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int total = 0;
+	int a = 1;
+	int b = 2;
+	int c = 0;
+ 
+	while (b <= 4000000)
+	{
+		c = a + b;
+		if (b % 2 == 0)
+			total += b;
+		a = b;
+		b = c;
+	}
+ 
+	cout << total;
+ 
+	return 0;
+}
